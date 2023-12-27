5d849546e1765b5df279436481f4208b89de3657
aguilarcarboni11 <aguilarcarboni@gmail.com>
2023-12-27 12:31:09 -0600

    Update layout

--- src/components/routes/Desktop/Home/Home.js
+++ src/components/routes/Desktop/Home/Home.js
@@ -8,16 +8,20 @@ const Home = () => {
   return (
     <div className='homeContainer'>
       <div className='intro'>
-        <h2 className='title'>Who is Perseverance?</h2> 
-        <h3 className='subtitle'>Perseverance, or Perse, is a rover sent out by NASA to Mars. Similar to any other rover we have sent in the past, Perse roams around Mars completing his mission and recollecting data.</h3>
-        <Link className='link' to="/aboutD">
-          <h4 className='subtitle' href=''>Learn more.</h4>
-        </Link>
-        <MarsMap height={'60%'} width={'100%'}/>
+        <div className='info'>
+          <h2 className='title'>Who is Perseverance?</h2> 
+          <h3 className='subtitle'>Perseverance, or Perse, is a rover sent out by NASA to Mars. Similar to any other rover we have sent in the past, Perse roams around Mars completing his mission and recollecting data.</h3>
+          <Link className='link' to="/aboutD">
+            <h4 className='subtitle' href=''>Learn more.</h4>
+          </Link>
+        </div>
+        <MarsMap height={'100%'} width={'100%'}/>
       </div>
       <div className='recentPhoto'>
         <h2 className='title'>Most recent photo</h2>
-        <MostRecentPhoto /> 
+        <div className='photoContainer'>
+          <MostRecentPhoto /> 
+        </div>
       </div>
     </div>
   )
